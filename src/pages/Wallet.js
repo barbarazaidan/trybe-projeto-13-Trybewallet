ed88f57201f4b9747fdd800aa98bec0289cb0d0b
Bárbara <babivaldez@gmail.com>
2023-02-05 20:57:58 -0300

    finaliza requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -8,6 +8,21 @@ class Wallet extends React.Component {
       <div className="walletDiv">
         <Header />
         <WalletForm />
+        <div className="walletTableDiv">
+          <table>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
